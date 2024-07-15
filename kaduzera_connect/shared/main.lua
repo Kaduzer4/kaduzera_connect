359649379d5d6e03b3718e490b4afd606cd0c7c3
Kaduzer4 <144576876+Kaduzer4@users.noreply.github.com>
2024-07-15 08:21:58 -0300

    Add files via upload

---
+++ kaduzera_connect/shared/main.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.Debug = false -- mandar mensagem para CMD? true = SIM
+
+Config.Webhook = '' -- copie e cole sua webhook
+Config.ServerName = '' -- Titulo da Msg
+
+Config.MessageId = '' -- inicie o resource sem ID, após iniciar, copie o id da mensagem e cole aqui.
+
+Config.UpdateTime = 1 -- tempo para atualizar (em minutos)
+Config.Use24hClock = true -- true= 24h / false= 12h (am/pm)
+Config.Connect = '' -- connect da cidade
+
+Config.EmbedColor = 2303786
+Config.WebhookIcon = ''
